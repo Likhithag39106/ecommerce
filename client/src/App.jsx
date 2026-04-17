11102f08e72e8de2afc9774314f1d18fd44bd4e6
Likhithag39106 <238w1a12g3@vrsec.ac.in>
2026-04-17 14:46:43 +0530

    cart-functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,18 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
 import Login from './components/Login'
-import Home from './components/Home'  
 import Addproduct from './components/Addproduct'
+import Home from './components/Home'
 import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
+
+
 export default function App() {
   return (
     <BrowserRouter>
     <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>}
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path="/add-product" element={
@@ -19,6 +22,12 @@ export default function App() {
             <Addproduct/>
           </ProtectedRoute>
         }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
+          </ProtectedRoute>
+        }/>
+
       </Routes>
     </BrowserRouter>
   )
